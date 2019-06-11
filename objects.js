d80396fd4939c0a6eada89b691babca917cf46f9
Alethia Quintero <alethiaq22@gmail.com>
2019-06-11 22:37:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {adele: "River Lea"};
 
 function updatePlaylist(obj, artistName, songTitle){
-  return Object.assign({}, obj, {artistName: songTitle})
+  return Object.assign({}, obj, {[artistName]: songTitle})
 }
