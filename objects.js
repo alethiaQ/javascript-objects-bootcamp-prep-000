b8b2a2c5179c166dfa1a212e90d0212125f29446
Alethia Quintero <alethiaq22@gmail.com>
2019-06-12 00:13:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,8 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle){
    return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
-playlist = delete playlist.artistName;
+var newPlaylist= delete playlist.artistName;
 
-  return playlist;
+  return newPlaylist;
 }
 
